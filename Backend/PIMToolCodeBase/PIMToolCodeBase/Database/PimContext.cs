7ea729008b535d1c7c4dc5251ffc35ca10e24dd1
TranPhucAnh <anh.tranphuc@elca.vn>
2023-06-20 17:12:16 +0700

    Init

---
+++ Backend/PIMToolCodeBase/PIMToolCodeBase/Database/PimContext.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Common;
+using System.Data.Entity;
+using PIMToolCodeBase.Domain.Entities;
+using PIMToolCodeBase.Extensions;
+
+namespace PIMToolCodeBase.Database
+{
+    /// <summary>
+    ///     DbContext of PIMTool.
+    /// </summary>
+    public class PimContext : DbContext
+    {
+        public PimContext() : base("PimDatabase")
+        {
+        }
+
+        public PimContext(DbConnection dbConnection) : base(dbConnection, true)
+        {
+        }
+
+        public DbSet<Sample> Samples { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BaseEntity>().HasKey(x => x.Id)
+                .Property(x => x.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            modelBuilder.Entity<Sample>().ToTablePerConcreteTable();
+        }
+    }
+}
